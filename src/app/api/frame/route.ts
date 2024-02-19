218530e78987b47e709ba9d5156ceb90581ad7fa
Shlok Khemani <khemanishlok@gmail.com>
2024-02-19 15:06:40 +0530

    check mint

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -7,7 +7,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   <meta property="fc:frame" content="vNext" />
   <meta property="fc:frame:image" content="https://diet-coke-frame.vercel.app/img-2.png" />
   <meta property="fc:frame:button:1" content="Mint" />
-  <meta property="fc:frame:post_url" content="https://zora.com" />
+  <meta property="fc:frame:button:1:action" content="mint" />
+  <meta property="fc:frame:button:1:target" content="eip155:7777777:0xd19b6313bc28722b9e5eaa4c1c41714e14dbce5b:3" />
 </head></html>`);
 }
 
@@ -16,3 +17,4 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
 }
 
 export const dynamic = "force-dynamic";
+// <meta property="fc:frame:post_url" content="https://diet-coke-frame.vercel.app/api/frame" />
