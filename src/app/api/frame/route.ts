ee2a9550396d37e71b548ab8e7a958cb540ce637
Shlok Khemani <khemanishlok@gmail.com>
2024-02-19 14:07:15 +0530

    first

---
+++ src/app/api/frame/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  const { pathname } = req.nextUrl;
+
+  return new NextResponse(`<!DOCTYPE html><html><head>
+  <meta property="fc:frame" content="vNext" />
+  <meta property="fc:frame:image" content="https://zizzamia.xyz/img-2.png" />
+  <meta property="fc:frame:button:1" content="Mint" />
+  <meta property="fc:frame:post_url" content="https://zora.com" />
+</head></html>`);
+}
+
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  return getResponse(req);
+}
+
+export const dynamic = "force-dynamic";
